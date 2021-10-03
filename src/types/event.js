332dd62ce8ab5b5056a28612c1c1a158e45f4918
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-10-03 14:18:50 -0400

    Event setup

---
+++ src/types/event.js
@@ -0,0 +1,29 @@
+const graphql = require('graphql')
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLDate, GraphQLList, GraphQLInputObjectType } = graphql
+
+const eventType = new GraphQLObjectType({
+  name: 'event',
+  type: 'Mutation',
+  fields: {
+    id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    date: { type: GraphQLDate },
+  }
+})
+
+const eventInput = new GraphQLInputObjectType({
+  name: 'eventInput',
+  type: 'Input',
+  fields: {
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    date: { type: GraphQLDate },
+  }
+})
+
+const eventsType = new GraphQLList(eventType)
+
+exports.eventType = eventType
+exports.eventInput = eventInput
+exports.eventsType = eventsType
